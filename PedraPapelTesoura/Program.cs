0efd3e40e73efc7762092e993940e7713d0b329d
Allan Gabriel <allangabrielferreirajeremias@gmail.com>
2024-09-19 14:08:42 -0300

    alteração de arquivo

--- PedraPapelTesoura/Program.cs
+++ PedraPapelTesoura/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PedraPapelTesoura
+{
+    class Program
+    {
+        static void Main(string[] args) // Corrigido 'sting' para 'string'
+        {
+            Random random = new Random();
+            string[] opcoes = { "pedra", "papel", "tesoura" };
+            string escolhaUsuario;
+            string continuar;
+
+            do
+            {
+                Console.WriteLine("Escolha uma opcao: pedra, papel ou tesoura");
+                escolhaUsuario = Console.ReadLine().ToLower(); // Corrigido 'escolaUsuario' para 'escolhaUsuario'
+
+                if (Array.Exists(opcoes, opcao => opcao == escolhaUsuario))
+                {
+                    int escolhaComputadorIndex = random.Next(0, 3); // Corrigido 'escolhaCompuradorIdex' para 'escolhaComputadorIndex'
+                    string escolhaComputador = opcoes[escolhaComputadorIndex];
+
+                    Console.WriteLine($"O computador escolheu: {escolhaComputador}"); // Corrigido 'escolhaCoputador' para 'escolhaComputador'
+
+                    if (escolhaUsuario == escolhaComputador)
+                    {
+                        Console.WriteLine("Empate!");
+                    }
+                    else if ((escolhaUsuario == "pedra" && escolhaComputador == "tesoura") || 
+                             (escolhaUsuario == "papel" && escolhaComputador == "pedra") || // Corrigido para 'papel' ganha de 'pedra'
+                             (escolhaUsuario == "tesoura" && escolhaComputador == "papel")) // Adicionado caso para 'tesoura'
+                    {
+                        Console.WriteLine("Voce ganhou!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Voce perdeu!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Opcao invalida! Tente novamente!"); // Corrigido a estrutura 'else'
+                }
+
+                Console.WriteLine("Deseja jogar novamente? (s/n)");
+                continuar = Console.ReadLine().ToLower();
+
+            } while (continuar == "s");
+
+            Console.WriteLine("Obrigado por jogar!");
+        }
+    }
+}
